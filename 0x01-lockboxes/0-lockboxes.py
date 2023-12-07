e4ce515a8857385a3c4e81fb7c7205d264bcb330
MesfinGithinji <meshgithinji@gmail.com>
2023-12-07 23:28:42 +0300

    Lock boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-'''How to open the lockboxes.
+'''A module for working with lockboxes.
 '''
 
 
 def canUnlockAll(boxes):
-    '''Check if all the boxes in a list of boxes containing the keys
-    (indices) to other boxes can be unlocked. The first Box is unlocked
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
     '''
     n = len(boxes)
     seen_boxes = set([0])
